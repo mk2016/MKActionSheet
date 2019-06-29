cbab8e122c1e722bbb11379a91a9f7d968a1d482
MK Xiao <xiaomk7758@sina.com>
2019-06-29 16:14:10 +0800

    ignore Pods

--- Pods/Headers/Private/MKFPSStatus/MKFPSStatus.h
+++
@@ -1,1 +0,0 @@
-../../../MKFPSStatus/MKFPSStatus/MKFPSStatus/MKFPSStatus.h
--- Pods/Target Support Files/MKFPSStatus/MKFPSStatus-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MKFPSStatus : NSObject
-@end
-@implementation PodsDummy_MKFPSStatus
-@end
